d04b92a92bcf53eee68bbc3fab16b0916be18e54
Stefan Rinke <sker65@gmail.com>
2021-08-02 14:25:47 +0200

    - added test server for web UI

---
+++ testserver/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+//const router = express.Router();
+
+app.use(express.static('../data', {fallthrough: true}));
+
+const config = {
+  itIs: 1,
+  rainbow: 1,
+  colors: "255,128,128,40,40,40,56,56,56",
+  mode: "1",
+  timezone: "2",
+  heartbeat: 0,
+  ntpserver: "129.3.2.2"
+};
+
+app.get('/:cmd', function (req, res) {
+  let what = req.params.cmd.substr(3);
+  if( req.params.cmd.startsWith('get') ) {
+    if( what === 'var') {
+      what = req.query.name;
+      console.log("request for name: "+what+ " -> "+config[what]);
+      res.status(200).send(config[what].toString());
+    } else {
+      console.log("request for name: "+what+ " -> "+config[what]);
+      res.status(200).send(config[what].toString());
+    }
+  } else if(req.params.cmd.startsWith('set')) {
+    res.status(200).send('OK');
+  }
+})
+
+console.log("server is listening on 3000");
+app.listen(3000)
